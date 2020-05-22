cd85e6deb7436d23cde1f2f2b45c112fcdb7c494
RitaEhab <36342037+RitaEhab@users.noreply.github.com>
2020-05-23 00:18:31 +0200

    Update uart.h

--- Tivac version for testing/uart.h
+++ Tivac version for testing/uart.h
@@ -1,3 +1,20 @@
-void InitConsole(void);
+#ifndef __UART_E_H__
+#define	__UART_E_H__
 
+#include "tm4c123gh6pm.h"
+
+void UART_Init(uint8 index);
+uint8 UART_Available(uint8 index);
+uint8 UART_Read(uint8 index);
+void UART_Write(uint8 index,uint8 data);
+void UART_sendString(const uint8 *Str);
+
+void UART_receiveString(uint8 *Str);
+
+
+#endif // __UART_H__
+
+/*
+void InitConsole(void);
 void ConfigureUART(void);
+*/
