27ca252d25c954d15a5f6c2c7dcd422976559df2
RitaEhab <36342037+RitaEhab@users.noreply.github.com>
2020-05-23 00:24:42 +0200

    added interface with putty

--- Tivac version for testing/main.c
+++ Tivac version for testing/main.c
@@ -2,6 +2,7 @@
 #include "CanIf.h"
 #include "PduR.h"
 #include "Com.h"
+#include "uart.h"
 
 int main()
 {
@@ -10,6 +11,13 @@ int main()
     PduR_Init(&pduR_PBConfigType);
     CanIf_Init(&canIf_ConfigType);
     Can_Init(&canConfigSet);
-
+    
+    UART_Init(7);
+    
+    while (1)
+    {
+        uint8 data = UART_Read(7);              // Recieve data from putty
+        uint8 ret = Com_SendSignal(0, &data);   // Send data to Com
+    }
     return 0;
 }
